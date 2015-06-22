ab1eeedcf25046a88b7a20810cb4c3aa2e1b9841
pururav thoutireddy <puru@pururavs-iMac.local>
2015-06-21 17:47:32 -0700

    first commit

---
+++ insert.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+#import enable
+#from enable.api import Component, ComponentEditor
+
+import mysql.connector
+import urllib
+
+base_url = "http://ichart.finance.yahoo.com/table.csv?s="
+output_path = '.'
+
+
+cnx = mysql.connector.connect(user='root', password='sakshi', host='localhost', database='test')
+print "Opened MySQL connection"
+cursor = cnx.cursor()
+cursor.execute("SELECT * FROM stockprices")
+print "Associated cursor"
+data = cursor.fetchone();
+while data is not None:
+  print data
+  data = cursor.fetchone();
+
+  
+  
+print "Closing MySQL connection"
+cnx.close()
+
+
+def make_url(ticker_symbol):
+    return base_url + ticker_symbol
+
+def make_filename(ticker_symbol, directory="data"):
+    return output_path + "/" + directory + "/" + ticker_symbol + ".csv"
+
+def pull_historical_data(ticker_symbol, directory="SP"):
+    try:
+        urllib.urlretrieve(make_url(ticker_symbol), make_filename(ticker_symbol, directory))
+    except urllib.ContentTooShortError as e:
+        outfile = open(make_filename(ticker_symbol, directory), "w")
+        outfile.write(e.content)
+        outfile.close()
